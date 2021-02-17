98da6ef50e5e5844f1ac8fba0583cd1be9e24081
AngurajDinesh <angurajdineshb@gmail.com>
2021-02-17 21:54:33 +0530

    homefinal

--- homepage.php
+++ homepage.php
@@ -6,11 +6,11 @@ require_once "head-side.php";
 <div class="row row-cols-1 row-cols-md-3 g-4">
 
     <div class="col">
-    <div class="card h-100 w-100  rounded">
+    <div class="card h-100 w-100 hover-shadow rounded">
         <div class="bg-image hover-zoom hover-overlay ripple shadow-1-strong rounded" data-mdb-ripple-duration="3s">
         <img src="images/c.png" class="card-img-top" alt="Sorry, Image Error"/>
         </div>
-        <div class="card-body hover-shadow">
+        <div class="card-body">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">This is a short card.</p>
         </div>
@@ -18,11 +18,11 @@ require_once "head-side.php";
     </div>
     
     <div class="col">
-    <div class="card h-100 w-100 rounded">
+    <div class="card h-100 w-100 hover-shadow rounded">
         <div class="bg-image hover-zoom hover-overlay ripple shadow-1-strong rounded" data-mdb-ripple-duration="3s">
         <img src="images/cpp.png" class="card-img-top" alt="Sorry, Image Error"/>
         </div>
-        <div class="card-body hover-shadow">
+        <div class="card-body">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">This is a short card.</p>
         </div>
@@ -30,11 +30,11 @@ require_once "head-side.php";
     </div>
     
     <div class="col">
-    <div class="card h-100 w-100 rounded">
+    <div class="card h-100 w-100 hover-shadow rounded">
         <div class="bg-image hover-zoom hover-overlay ripple shadow-1-strong rounded" data-mdb-ripple-duration="3s">
         <img src="images/java.png" class="card-img-top" alt="Sorry, Image Error"/>
         </div>
-        <div class="card-body hover-shadow">
+        <div class="card-body">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">This is a short card.</p>
         </div>
@@ -42,11 +42,11 @@ require_once "head-side.php";
     </div>
 
     <div class="col">
-    <div class="card h-100 w-100 rounded">
+    <div class="card h-100 w-100 hover-shadow rounded">
         <div class="bg-image hover-zoom hover-overlay ripple shadow-1-strong rounded" data-mdb-ripple-duration="3s">
         <img src="images/python.png" class="card-img-top" alt="Sorry, Image Error"/>
         </div>
-        <div class="card-body hover-shadow">
+        <div class="card-body">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">This is a short card.</p>
         </div>
--- summa.php
+++ summa.php
@@ -6,12 +6,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <title>Angu</title>
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
   <!-- Bootstrap core CSS -->
   <link rel="stylesheet" href="mdb/css/bootstrap.min.css">
   <!-- Material Design Bootstrap -->
   <link rel="stylesheet" href="mdb/css/mdb.min.css">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 
   <!-- Font Awesome -->
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet"/>
@@ -265,6 +266,9 @@
 
 <!-- MDB -->
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.2.0/mdb.min.js"></script>
+
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 
 <script>
   // SideNav Initialization
