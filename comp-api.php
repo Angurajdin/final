c2f194d80d416fe95aa4840fbccaf4c9f6f668dd
AngurajDinesh <angurajdineshb@gmail.com>
2021-02-26 17:35:20 +0530

    complier added

---
+++ comp-api.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html> 
+<html> 
+
+<body> 
+	<!-- Form to redirect the page 
+		to C online compiler -->
+	<form method="post" action= "https://www.jdoodle.com/api/redirect-to-post/c-online-compiler"> 
+		
+		<textarea name="initScript" rows="8" cols="80"> </textarea> 
+
+		<input type="submit" value="Test"> 
+	</form> 
+</body> 
+
+</html> 
